3aa8e91f98e2fd93aa782d228f476d64d2ac05e1
Samriddhi Makasare <147656319+samriddhimakasare25@users.noreply.github.com>
2024-11-04 17:07:14 -0500

    main

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -3,10 +3,18 @@ import client from '@/config/postmark';
 export async function POST(request: Request) {
   const body = await request.json();
   try {
+    const fromEmail = process.env.EMAIL_FROM as string; // Type assertion
+    const toEmail = process.env.EMAIL_TO as string; // Type assertion
+
+    // Optional: Add checks to throw an error if the environment variables are not defined
+    if (!fromEmail || !toEmail) {
+      throw new Error('Environment variables EMAIL_FROM and EMAIL_TO must be set.');
+    }
+
     await client.sendEmail({
-      From: process.env.EMAIL_FROM,
-      To: process.env.EMAIL_TO,
-      Subject: 'Porfolio Contact Form',
+      From: fromEmail,
+      To: toEmail,
+      Subject: 'Portfolio Contact Form',
       HtmlBody: `
         <h1>Portfolio Contact Form</h1>
         <p><strong>Name:</strong> ${body.person_name}</p>
