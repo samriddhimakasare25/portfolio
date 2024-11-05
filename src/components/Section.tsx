f5dfd0f57f2da390767ee4f5d342f301044c9e1a
Samriddhi Makasare <147656319+samriddhimakasare25@users.noreply.github.com>
2024-11-04 21:40:21 -0500

    yep

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -3,19 +3,27 @@ import { sectionSlice, useDispatch } from '@/lib/redux';
 import { useInView } from 'framer-motion';
 import { useEffect, useRef } from 'react';
 
-export default function Section({ children, id, className }: { children: React.ReactNode; id: string; className?: string }) {
+interface SectionProps {
+  children: React.ReactNode;
+  id: string;
+  className?: string;
+}
+
+export default function Section({ children, id, className }: SectionProps) {
   const ref = useRef<HTMLElement>(null);
   const isInView = useInView(ref);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (!ref.current) return;
+
+    // Dispatch the appropriate action based on visibility
     if (isInView) {
       dispatch(sectionSlice.actions.setVisible({ key: ref.current.id }));
     } else {
       dispatch(sectionSlice.actions.setHidden({ key: ref.current.id }));
     }
-  }, [ref, isInView, dispatch]);
+  }, [isInView, dispatch]); // No need to include ref as a dependency
 
   return (
     <section id={id} ref={ref} className={className}>
